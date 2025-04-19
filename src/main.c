99aa7248de3ec08b98b90c1f262f4f8bb871b898
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-04-19 14:31:02 +0200

    Checking argc has a valid value

--- src/main.c
+++ src/main.c
@@ -1,5 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Maximum number of command line arguments.
+#define GN_MAX_CMDL_ARGS 8
+
 int
-main (const int argc, const char * const * const argv)
+main (const int argc,
+      __attribute__((unused)) const char * const * const argv)
 {
-    return 0;
+    // Checking @argc.
+    if (argc < 0)
+    {
+        fprintf (stderr, "Negative number (%i) of command line arguments.\n", argc);
+        return EXIT_FAILURE;
+    }
+    if (argc > GN_MAX_CMDL_ARGS)
+    {
+        fprintf (stderr, "Number of command line arguments (%i) too high (maximum %i).\n", argc, GN_MAX_CMDL_ARGS);
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
