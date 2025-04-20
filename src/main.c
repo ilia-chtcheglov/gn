deaf920a996b5b8c304877c1ac4014c2d3036dae
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-04-20 11:23:33 +0200

    Pushing server socket struct to linked list

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <arpa/inet.h>
 #include <errno.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,18 +19,58 @@ struct gn_serv_sock_t
     gn_serv_sock_t * next;
 };
 
-__attribute__((warn_unused_result))
-gn_serv_sock_t *
-gn_open_serv_sock (const char * const addr, const uint16_t port);
+typedef struct gn_serv_sock_list_t gn_serv_sock_list_t;
+
+struct gn_serv_sock_list_t
+{
+    gn_serv_sock_t * head;
+    gn_serv_sock_t * tail;
+    size_t           len;
+};
+
+int
+gn_serv_sock_list_push_back (gn_serv_sock_list_t * const list, gn_serv_sock_t * const sock);
+
+int
+gn_serv_sock_list_push_back (gn_serv_sock_list_t * const list, gn_serv_sock_t * const sock)
+{
+    switch (list->len)
+    {
+        case 0:
+        {
+            list->head = list->tail = sock->prev = sock->next = sock;
+            break;
+        }
+        case SIZE_MAX:
+        {
+            return 1;
+        }
+        default:
+        {
+            list->tail->next = sock;
+            sock->prev = list->tail;
+            list->head->prev = sock;
+            sock->next = list->head;
+            list->tail = sock;
+        }
+    }
+
+    list->len++;
+    return 0;
+}
 
 __attribute__((warn_unused_result))
-gn_serv_sock_t *
-gn_open_serv_sock (const char * const addr, const uint16_t port)
+int
+gn_open_serv_sock (gn_serv_sock_list_t * const list, const char * const addr, const uint16_t port);
+
+__attribute__((warn_unused_result))
+int
+gn_open_serv_sock (gn_serv_sock_list_t * const list, const char * const addr, const uint16_t port)
 {
-    if (port < 1) return NULL;
+    if (port < 1) return 1;
 
     gn_serv_sock_t * serv_sock = (gn_serv_sock_t *)malloc (sizeof (gn_serv_sock_t));
-    if (serv_sock == NULL) return NULL;
+    if (serv_sock == NULL) return 1;
 
     const int rsocket = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (rsocket > -1)
@@ -52,7 +93,8 @@ gn_open_serv_sock (const char * const addr, const uint16_t port)
                     case 0:
                     {
                         serv_sock->fd = rsocket;
-                        return serv_sock;
+                        gn_serv_sock_list_push_back (list, serv_sock);
+                        return 0;
                     }
                     case -1:
                     {
@@ -85,7 +127,7 @@ gn_open_serv_sock (const char * const addr, const uint16_t port)
     }
 
     free (serv_sock);
-    return NULL;
+    return 1;
 }
 
 int
@@ -104,18 +146,15 @@ main (const int argc,
         return EXIT_FAILURE;
     }
 
-    gn_serv_sock_t * serv_sock = gn_open_serv_sock ("0.0.0.0", 8080);
-    if (serv_sock != NULL)
-    {
-        close (serv_sock->fd);
-        free (serv_sock);
-    }
-    serv_sock = gn_open_serv_sock ("127.0.0.1", 8081);
-    if (serv_sock != NULL)
-    {
-        close (serv_sock->fd);
-        free (serv_sock);
-    }
+    gn_serv_sock_list_t serv_sock_list;
+    memset (&serv_sock_list, 0, sizeof (gn_serv_sock_list_t));
+
+    int rgn_open_serv_sock = gn_open_serv_sock (&serv_sock_list, "0.0.0.0", 8080);
+    if (rgn_open_serv_sock != 0) fprintf (stderr, "Failed to open server socket.\n");
+    rgn_open_serv_sock = gn_open_serv_sock (&serv_sock_list, "127.0.0.1", 8081);
+    if (rgn_open_serv_sock != 0) fprintf (stderr, "Failed to open server socket.\n");
+
+    // TODO: Empty @serv_sock_list and close sockets.
 
     return EXIT_SUCCESS;
 }
