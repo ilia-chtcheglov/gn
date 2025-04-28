557f9c1227d72cf5c997c8a955d9d8e6526076b9
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-04-28 15:51:47 +0200

    Moved function to new file

--- src/mstr/gn_mstr_main.c
+++ src/mstr/gn_mstr_main.c
@@ -12,91 +12,9 @@
 
 #include <gn_serv_sock_list_t.h>
 
-int
-gn_serv_sock_list_push_back (gn_serv_sock_list_t * const list, gn_serv_sock_t * const sock);
-
 __attribute__((warn_unused_result))
 int
 gn_open_serv_sock (gn_serv_sock_list_t * const list, const char * const addr, const uint16_t port);
-
-__attribute__((warn_unused_result))
-int
-gn_open_serv_sock (gn_serv_sock_list_t * const list, const char * const addr, const uint16_t port)
-{
-    if (port < 1) return 1;
-
-    gn_serv_sock_t * serv_sock = (gn_serv_sock_t *)malloc (sizeof (gn_serv_sock_t));
-    if (serv_sock == NULL) return 1;
-
-    serv_sock->addr = (char *)malloc (strlen (addr) + 1);
-    if (serv_sock->addr == NULL)
-    {
-        free (serv_sock);
-        return 1;
-    }
-
-    const int rsocket = socket (AF_INET, SOCK_STREAM | SOCK_CLOEXEC | SOCK_NONBLOCK, IPPROTO_TCP);
-    if (rsocket > -1)
-    {
-        printf ("Opened FD %i.\n", rsocket);
-        struct sockaddr_in sin;
-        memset (&sin, 0, sizeof (sin));
-
-        sin.sin_addr.s_addr = inet_addr (addr);
-        sin.sin_family = AF_INET;
-        sin.sin_port = htons (port);
-
-        const int rbind = bind (rsocket, (struct sockaddr *)&sin, sizeof (sin));
-        switch (rbind)
-        {
-            case 0:
-            {
-                const int rlisten = listen (rsocket, SOMAXCONN);
-                switch (rlisten)
-                {
-                    case 0:
-                    {
-                        serv_sock->fd = rsocket;
-                        strcpy (serv_sock->addr, addr);
-                        serv_sock->port = port;
-
-                        gn_serv_sock_list_push_back (list, serv_sock);
-                        return 0;
-                    }
-                    case -1:
-                    {
-                        fprintf (stderr, "Failed to listen on socket. %s.\n", strerror (errno));
-                        break;
-                    }
-                    default:
-                    {
-                        fprintf (stderr, "listen() returned unexpected value %i.\n", rlisten);
-                    }
-                }
-                break;
-            }
-            case -1:
-            {
-                fprintf (stderr, "Failed to bind socket. %s.\n", strerror (errno));
-                break;
-            }
-            default:
-            {
-                fprintf (stderr, "bind() returned unexpected value %i.\n", rbind);
-            }
-        }
-
-        close (rsocket);
-    }
-    else
-    {
-        fprintf (stderr, "Failed to create socket. %s.\n", strerror (errno));
-    }
-
-    free (serv_sock->addr);
-    free (serv_sock);
-    return 1;
-}
 
 void
 gn_send_serv_sock (const int ipc_sock, const gn_serv_sock_t * const serv_sock);
---
+++ src/mstr/include/gn_open_serv_sock.h
@@ -0,0 +1,20 @@
+#ifndef GN_OPEN_SERV_SOCK_H
+#define GN_OPEN_SERV_SOCK_H
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <gn_serv_sock_list_t.h>
+
+__attribute__((warn_unused_result))
+int
+gn_open_serv_sock (gn_serv_sock_list_t * const list, const char * const addr, const uint16_t port);
+
+int
+gn_serv_sock_list_push_back (gn_serv_sock_list_t * const list, gn_serv_sock_t * const sock);
+
+#endif // GN_OPEN_SERV_SOCK_H
