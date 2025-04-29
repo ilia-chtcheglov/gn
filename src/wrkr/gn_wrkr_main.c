56d4cacdd5bb4980079e36433196a54bb5ec7431
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-04-29 15:38:57 +0200

    Moved code to new function

--- src/wrkr/gn_wrkr_main.c
+++ src/wrkr/gn_wrkr_main.c
@@ -1,10 +1,13 @@
 #include <gn_wrkr_main.h>
 
-void
-gn_wrkr_main (int ipc_sock, gn_serv_sock_list_t * const serv_sock_list, const char * const ipc_addr_str)
-{
-    printf ("[%i] Worker started. Received IPC address \"%s\".\n", getpid (), ipc_addr_str);
+__attribute__((warn_unused_result))
+int
+gn_ipc_conn (const int ipc_sock, const char * const ipc_addr_str);
 
+__attribute__((warn_unused_result))
+int
+gn_ipc_conn (const int ipc_sock, const char * const ipc_addr_str)
+{
     struct sockaddr_un sun;
     memset (&sun, 0, sizeof (sun));
 
@@ -17,44 +20,52 @@ gn_wrkr_main (int ipc_sock, gn_serv_sock_list_t * const serv_sock_list, const ch
     {
         case 0:
         {
-            printf ("Connected to master process.\n");
-
-            // Receive server sockets data.
-            int rgn_recv_serv_sock = 0;
-            while (rgn_recv_serv_sock == 0)
-            {
-                rgn_recv_serv_sock = gn_recv_serv_sock (ipc_sock, serv_sock_list);
-            }
-
-            if (rgn_recv_serv_sock == 1)
-            {
-                // Test code.
-                const gn_serv_sock_t * serv_sock = serv_sock_list->head;
-                for (uint16_t i = 0; i < serv_sock_list->len; serv_sock = serv_sock->next, i++)
-                {
-                    printf ("Received server socket .fd: %i, .addr: [%s], .port: %i.\n",
-                            serv_sock->fd, serv_sock->addr, serv_sock->port);
-                }
-
-                // Main loop.
-                while (true) sleep (1);
-            }
-            else
-            {
-                fprintf (stderr, "Error occured while receiving server socket data.\n");
-            }
-
-            break;
+            printf ("Connected to master process.\n"); // TODO: Remove.
+            return EXIT_SUCCESS;
         }
         case -1:
         {
             fprintf (stderr, "Failed to connect to master process. %s.\n", strerror (errno));
-            return;
+            return EXIT_FAILURE;
         }
         default:
         {
             fprintf (stderr, "connect() returned unexpected value %i.\n", rconnect);
-            return;
+            return EXIT_FAILURE;
         }
+    }
+}
+
+void
+gn_wrkr_main (int ipc_sock, gn_serv_sock_list_t * const serv_sock_list, const char * const ipc_addr_str)
+{
+    printf ("[%i] Worker started. Received IPC address \"%s\".\n", getpid (), ipc_addr_str);
+
+    // Connect to the master process.
+    if (gn_ipc_conn (ipc_sock, ipc_addr_str) != EXIT_SUCCESS) return;
+
+    // Receive server sockets data.
+    int rgn_recv_serv_sock = 0;
+    while (rgn_recv_serv_sock == 0)
+    {
+        rgn_recv_serv_sock = gn_recv_serv_sock (ipc_sock, serv_sock_list);
+    }
+
+    if (rgn_recv_serv_sock == 1)
+    {
+        // Test code.
+        const gn_serv_sock_t * serv_sock = serv_sock_list->head;
+        for (uint16_t i = 0; i < serv_sock_list->len; serv_sock = serv_sock->next, i++)
+        {
+            printf ("Received server socket .fd: %i, .addr: [%s], .port: %i.\n",
+                    serv_sock->fd, serv_sock->addr, serv_sock->port);
+        }
+
+        // Main loop.
+        while (true) sleep (1);
+    }
+    else
+    {
+        fprintf (stderr, "Error occured while receiving server socket data.\n");
     }
 }
