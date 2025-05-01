918b87b01a94c720af8e5d714c39fff50ed8af73
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-05-01 09:46:35 +0200

    Wait for and accept connections on server sockets

--- src/wrkr/gn_wrkr_main.c
+++ src/wrkr/gn_wrkr_main.c
@@ -1,5 +1,7 @@
 #include <gn_wrkr_main.h>
 
+#include <arpa/inet.h>
+
 void
 gn_wrkr_main (int ipc_sock, gn_serv_sock_list_t * const serv_sock_list, const char * const ipc_addr_str)
 {
@@ -37,9 +39,71 @@ gn_wrkr_main (int ipc_sock, gn_serv_sock_list_t * const serv_sock_list, const ch
     }
 
     // Main loop.
-    while (true)
+    bool main_loop = true;
+    while (main_loop)
     {
-        sleep (1);
+        struct epoll_event epoll_evts[64];
+        memset (epoll_evts, 0, sizeof (epoll_evts));
+
+        const int repoll_wait = epoll_wait (repoll_create1, epoll_evts,
+                                            sizeof (epoll_evts) / sizeof (struct epoll_event), 1000);
+        switch (repoll_wait)
+        {
+            case 0:
+            {
+                // Timeout.
+                break;
+            }
+            case -1:
+            {
+                switch (errno)
+                {
+                    case EINTR:
+                        break;
+                    case EBADF:
+                    case EINVAL:
+                    {
+                        repoll_create1 = -1;
+                        __attribute__((fallthrough));
+                    }
+                    case EFAULT:
+                    {
+                        fprintf (stderr, "epoll_wait() failed. %s.\n", strerror (errno));
+                        main_loop = false;
+                        break;
+                    }
+                    default:
+                    {
+                        fprintf (stderr, "epoll_wait() returned undocumented errno code %i.\n", errno);
+                        main_loop = false;
+                    }
+                }
+                break;
+            }
+            default:
+            {
+                if (repoll_wait > 0)
+                {
+                    for (size_t i = 0; i < (size_t)repoll_wait; i++)
+                    {
+                        const struct gn_serv_sock_t * const serv_sock = (gn_serv_sock_t *)epoll_evts[i].data.ptr;
+                        int raccept4 = accept4 (serv_sock->fd, NULL, NULL, SOCK_CLOEXEC | SOCK_NONBLOCK);
+                        if (raccept4 > -1)
+                        {
+                            printf ("Accepted connection from X to [%s]:%i.\n", serv_sock->addr, serv_sock->port);
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+                else
+                {
+
+                }
+            }
+        }
     }
 
     close (repoll_create1);
