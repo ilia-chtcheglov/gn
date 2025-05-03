35b2866470cbe5653f31a96411ecaba87769fbc1
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-05-03 13:50:10 +0200

    Pass conn to connection management thread

--- src/wrkr/gn_acpt_conn.c
+++ src/wrkr/gn_acpt_conn.c
@@ -1,15 +1,6 @@
 #include <gn_acpt_conn.h>
 
-typedef struct gn_conn_t gn_conn_t;
-
-struct gn_conn_t
-{
-    int         fd;
-    char *      saddr;
-    uint16_t    sport;
-    gn_conn_t * prev;
-    gn_conn_t * next;
-};
+#include <stdatomic.h>
 
 __attribute__((warn_unused_result))
 int
@@ -20,32 +11,58 @@ gn_acpt_conn (const gn_serv_sock_t * const serv_sock, gn_conn_mgmt_thrd_data_lis
     memset (&sin, 0, sizeof (sin));
     socklen_t sin_sz = sizeof (sin);
 
+    // Accept connection.
     int raccept4 = accept4 (serv_sock->fd, &sin, &sin_sz, SOCK_CLOEXEC | SOCK_NONBLOCK);
     if (raccept4 > -1)
     {
+        // Contains the source IP.
         char saddr[INET_ADDRSTRLEN];
         memset (saddr, 0, sizeof (saddr));
 
+        // Get the source IP and store it in @saddr.
         const char * const rinet_ntop = inet_ntop (AF_INET, &sin.sin_addr, saddr, sizeof (saddr));
         if (rinet_ntop == NULL)
         {
             fprintf (stderr, "inet_ntop() failed. %s.\n", strerror (errno));
         }
 
+        // Get the source port.
         const uint16_t sport = ntohs (sin.sin_port);
         printf ("Accepted connection from [%s]:%i to [%s]:%i.\n", saddr, sport, serv_sock->addr, serv_sock->port);
 
+        // Allocate structure to store connection data (socket, state, etc).
         gn_conn_t * conn = (gn_conn_t *)malloc (sizeof (gn_conn_t));
         if (conn != NULL)
         {
+            printf ("Allocated conn: %p\n", (void *)conn);
             memset (conn, 0, sizeof (gn_conn_t));
+            // Allocate buffer to store the source IP.
             conn->saddr = (char *)malloc (strlen (saddr) + 1);
             if (conn->saddr != NULL)
             {
+                // Store data in the connection structure.
                 strcpy (conn->saddr, saddr);
                 conn->fd = raccept4;
                 conn->sport = sport;
-                free (conn->saddr);
+
+                // Pass the structure to a connection management thread.
+                const gn_conn_mgmt_thrd_data_t * data = list->head;
+                for (uint8_t i = 0; i < list->len; data = data->next, i++)
+                {
+                    atomic_uintptr_t expected = (atomic_uintptr_t)NULL;
+                    if (atomic_compare_exchange_strong_explicit (&data->conn, &expected, conn,
+                                                                 memory_order_relaxed, memory_order_relaxed))
+                    {
+                        conn = NULL;
+                        raccept4 = -1;
+                    }
+                    else
+                    {
+                        fprintf (stderr, "Failed to pass connection to connection management thread.\n");
+                    }
+                }
+
+                if (conn != NULL) free (conn->saddr);
             }
             else
             {
@@ -58,10 +75,12 @@ gn_acpt_conn (const gn_serv_sock_t * const serv_sock, gn_conn_mgmt_thrd_data_lis
             fprintf (stderr, "Failed to allocate structure for connection.\n");
         }
 
+        // Close client socket.
         close (raccept4);
     }
     else
     {
+        // Handle accept4() error.
         switch (errno)
         {
             case EAGAIN:
