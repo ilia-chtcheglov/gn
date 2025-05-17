a6b839497b07d9ffa76dbebdcbea60f01672dd6d
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-05-17 15:02:08 +0200

    Use atomic functions to get new connections

--- src/wrkr/gn_conn_mgmt_thrd.c
+++ src/wrkr/gn_conn_mgmt_thrd.c
@@ -70,6 +70,7 @@ gn_conn_mgmt_thrd (void * const p)
     gn_conn_mgmt_thrd_data_t * const data = (gn_conn_mgmt_thrd_data_t *)p;
     atomic_store_explicit (&data->state, GN_CONN_MGMT_THRD_STATE_RUNNING, memory_order_relaxed);
 
+    // Connections list.
     gn_conn_list_t conn_list;
     memset (&conn_list, 0, sizeof (conn_list));
 
@@ -77,6 +78,7 @@ gn_conn_mgmt_thrd (void * const p)
     bool main_loop = true;
     while (main_loop)
     {
+        // Process connections one by one.
         gn_conn_t * conn = conn_list.head;
         gn_conn_t * next_conn = NULL;
         for (uint32_t i = 0; i < conn_list.len; i++)
@@ -91,6 +93,7 @@ gn_conn_mgmt_thrd (void * const p)
                 printf ("Received (%lu) \"%s\"\n", buf_len, buf);
             }
 
+            // If the thread has to stop we close and remove the connection from the list.
             if (stop)
             {
                 gn_conn_list_remove (&conn_list, conn);
@@ -110,20 +113,34 @@ gn_conn_mgmt_thrd (void * const p)
             conn = next_conn;
         }
 
+        /*
+         * Get new connections from new_conns and add them to the connections list.
+         * new_conns is an array of atomic_uintptr_t variables.
+         */
         for (int8_t i = sizeof (data->new_conns) / sizeof (atomic_uintptr_t) - 1; i > -1 ; i--)
         {
-            if (data->new_conns[i] == (uintptr_t)NULL) continue;
+            // If there's no new connection at this index, try next index.
+            if (atomic_load_explicit (&data->new_conns[i], memory_order_relaxed) == (uintptr_t)NULL) continue;
 
+            // Turn the atomic_uintptr_t value into a pointer to a connection structure.
             gn_conn_t * const new_conn = (gn_conn_t *)data->new_conns[i];
+            // Close the connection if the thread has to stop or the connections list is full.
             if (stop || gn_conn_list_push_back (&conn_list, new_conn) != 0)
             {
                 gn_close (&new_conn->fd);
                 free (new_conn->saddr);
                 free (new_conn);
             }
-            data->new_conns[i] = (uintptr_t)NULL;
+
+            // Make the array index available for a new connection.
+            atomic_store_explicit (&data->new_conns[i], (uintptr_t)NULL, memory_order_relaxed);
         }
 
+        /*
+         * If the local stop variable isn't true we check the atomic stop variable.
+         * If the atomic one is true we set the local one to true and signal that the thread is stopping.
+         * If the thread doesn't have to stop we wait 1 second to avoid 100% CPU usage.
+         */
         if (!stop)
         {
             if (atomic_load_explicit (&data->stop, memory_order_relaxed))
@@ -134,9 +151,15 @@ gn_conn_mgmt_thrd (void * const p)
             }
             else sleep (1);
         }
-        else if (conn_list.len == 0) break;
+        else if (conn_list.len == 0)
+        {
+            // If the thread has to stop and the connection list is empty we leave the main loop.
+            main_loop = false;
+            break;
+        }
     }
 
+    // Signal that the thread is stopped.
     atomic_store_explicit (&data->state, GN_CONN_MGMT_THRD_STATE_STOPPED, memory_order_relaxed);
     return NULL;
 }
