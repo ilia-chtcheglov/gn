472330c377c75ea304cd4456e4ee25ffacc238cb
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-05-24 16:47:52 +0200

    Moved code to new functions

--- src/mstr/gn_mstr_init.c
+++ src/mstr/gn_mstr_init.c
@@ -99,37 +99,88 @@ gn_sigint_handler (const int signum)
     sigint_rcvd = true;
 }
 
-void
-gn_mstr_init (int ipc_sock, gn_serv_sock_list_t * const serv_sock_list)
-{
-    if (gn_set_sig_hndlr (SIGINT, gn_sigint_handler) != EXIT_SUCCESS) return;
+__attribute__((warn_unused_result))
+bool
+gn_ipc_lstn (const int ipc_sock);
 
-    // Get current time to generate a path for the IPC socket.
-    struct timespec ts;
-    memset (&ts, 0, sizeof (ts));
-    const int rclock_gettime = clock_gettime (CLOCK_REALTIME, &ts); // 0 OR -1 ERRNO
-    switch (rclock_gettime)
+__attribute__((warn_unused_result))
+bool
+gn_ipc_lstn (const int ipc_sock)
+{
+    const int rlisten = listen (ipc_sock, 8);
+    switch (rlisten)
     {
         case 0:
-        {
-            break;
-        }
+            return EXIT_SUCCESS;
         case -1:
         {
-            fprintf (stderr, "Failed to get current time. %s.\n", strerror (errno));
-            break;
+            fprintf (stderr, "Failed to listen on IPC socket. %s.\n", strerror (errno));
+            return EXIT_FAILURE;
         }
         default:
         {
-
+            fprintf (stderr, "listen() returned undocumented value %i.\n", rlisten);
+            return EXIT_FAILURE;
         }
     }
+}
 
-    struct sockaddr_un sun;
-    memset (&sun, 0, sizeof (sun));
+__attribute__((warn_unused_result))
+bool
+gn_ipc_bind (const int ipc_sock, struct sockaddr_un * const ipc_addr);
 
-    sun.sun_family = AF_UNIX;
-    // sun.sun_path is sun_path[108].
+__attribute__((warn_unused_result))
+bool
+gn_ipc_bind (const int ipc_sock, struct sockaddr_un * const ipc_addr)
+{
+    const int rbind = bind (ipc_sock, (struct sockaddr *)ipc_addr, sizeof (struct sockaddr_un));
+    switch (rbind)
+    {
+        case 0:
+            return EXIT_SUCCESS;
+        case -1:
+        {
+            fprintf (stderr, "Failed to bind IPC socket. %s.\n", strerror (errno));
+            return EXIT_FAILURE;
+        }
+        default:
+        {
+            fprintf (stderr, "bind() returned undocumented value %i.\n", rbind);
+            return EXIT_FAILURE;
+        }
+    }
+}
+
+__attribute__((malloc))
+__attribute__((warn_unused_result))
+char *
+gn_ipc_prep (const int ipc_sock);
+
+__attribute__((malloc))
+__attribute__((warn_unused_result))
+char *
+gn_ipc_prep (const int ipc_sock)
+{
+    // Get current time to generate a path for the IPC socket.
+    struct timespec ts;
+    memset (&ts, 0, sizeof (ts));
+
+    const int rclock_gettime = clock_gettime (CLOCK_REALTIME, &ts);
+    switch (rclock_gettime)
+    {
+        case 0:
+            break;
+        case -1:
+        {
+            fprintf (stderr, "Failed to get current time. %s.\n", strerror (errno));
+            return NULL;
+        }
+        default:
+        {
+            fprintf (stderr, "clock_gettime() returned undocumented value %i.\n", rclock_gettime);
+            return NULL;
+        }
+    }
 
     /*
      * Generate an address for the IPC socket. The address is made of:
@@ -138,6 +189,12 @@ gn_mstr_init (int ipc_sock, gn_serv_sock_list_t * const serv_sock_list)
      * - Nanoseconds in hexadecimal.
      * - The first byte will be replaced with '\0' to make the address an abstract one.
      */
+    struct sockaddr_un sun;
+    memset (&sun, 0, sizeof (sun));
+
+    sun.sun_family = AF_UNIX;
+    // sun.sun_path is sun_path[108].
+
     int rsnprintf = snprintf (sun.sun_path, sizeof (sun.sun_path), "Z%X%lX%lX",
                               (unsigned int)getpid (), (long unsigned int)ts.tv_sec, (long unsigned int)ts.tv_nsec);
     printf ("rsnprintf: %i, sun_path: \"%s\".\n", rsnprintf, sun.sun_path);
@@ -148,45 +205,31 @@ gn_mstr_init (int ipc_sock, gn_serv_sock_list_t * const serv_sock_list)
      */
     sun.sun_path[0] = '\0';
 
-    const int rbind = bind (ipc_sock, (struct sockaddr *)&sun, sizeof (sun));
-    switch (rbind)
-    {
-        case 0:
-        {
-            // Set sun_path[0] back to some byte because some gn_* functions can't work if @sun_path starts with '\0'.
-            sun.sun_path[0] = 'Z';
+    if (gn_ipc_bind (ipc_sock, &sun) != EXIT_SUCCESS) return NULL;
 
-            const int rlisten = listen (ipc_sock, 8);
-            switch (rlisten)
-            {
-                case 0:
-                {
-                    break;
-                }
-                case -1:
-                {
-                    fprintf (stderr, "Failed to listen on IPC socket. %s.\n", strerror (errno));
-                    return;
-                }
-                default:
-                {
-                    fprintf (stderr, "listen() returned unexpected value %i.\n", rlisten);
-                    return;
-                }
-            }
-            break;
-        }
-        case -1:
-        {
-            fprintf (stderr, "Failed to bind socket. %s.\n", strerror (errno));
-            return;
-        }
-        default:
-        {
-            fprintf (stderr, "bind() returned unexpected value %i.\n", rbind);
-            return;
-        }
+    // Set sun_path[0] back to some byte because some gn_* functions can't work if @sun_path starts with '\0'.
+    sun.sun_path[0] = 'Z';
+
+    if (gn_ipc_lstn (ipc_sock) != EXIT_SUCCESS) return NULL;
+
+    char * ipc_addr_str = (char *)malloc (strlen (sun.sun_path) + 1);
+    if (ipc_addr_str == NULL)
+    {
+        fprintf (stderr, "Failed to allocate buffer for IPC address string.\n");
+        return NULL;
     }
+
+    strcpy (ipc_addr_str, sun.sun_path);
+    return ipc_addr_str;
+}
+
+void
+gn_mstr_init (int ipc_sock, gn_serv_sock_list_t * const serv_sock_list)
+{
+    if (gn_set_sig_hndlr (SIGINT, gn_sigint_handler) != EXIT_SUCCESS) return;
+
+    char * ipc_addr_str = gn_ipc_prep (ipc_sock);
+    if (ipc_addr_str == NULL) return;
 
     gn_mstr_conf_t mstr_conf;
     mstr_conf.workers = 1;
@@ -244,14 +287,17 @@ gn_mstr_init (int ipc_sock, gn_serv_sock_list_t * const serv_sock_list)
         {
             // Start worker processes.
             gn_start_wrkrs (&wrkr_data_list, repoll_create1, mstr_conf.workers, self_path,
-                            ipc_sock, sun.sun_path, serv_sock_list);
+                            ipc_sock, ipc_addr_str, serv_sock_list);
         }
     }
 
-    gn_mstr_main (&wrkr_data_list, &repoll_create1, self_path, ipc_sock, sun.sun_path, serv_sock_list);
+    gn_mstr_main (&wrkr_data_list, &repoll_create1, self_path, ipc_sock, ipc_addr_str, serv_sock_list);
 
     // Stop worker processes.
     gn_stop_wrkrs (&wrkr_data_list);
 
     gn_close (&repoll_create1);
+
+    free (ipc_addr_str);
+    ipc_addr_str = NULL;
 }
