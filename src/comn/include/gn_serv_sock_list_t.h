6f897e9c92a4d1cb1c35296cc2db26f561721cae
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-05-27 15:52:12 +0200

    Added type and macro

--- src/comn/include/gn_serv_sock_list_t.h
+++ src/comn/include/gn_serv_sock_list_t.h
@@ -3,11 +3,14 @@
 
 #include <gn_serv_sock_t.h>
 
+typedef uint16_t gn_serv_sock_list_len_t;
+#define GN_SERV_SOCK_LIST_LEN_MAX UINT16_MAX
+
 typedef struct
 {
-    gn_serv_sock_t * head;
-    gn_serv_sock_t * tail;
-    uint16_t         len;
+    gn_serv_sock_t *        head;
+    gn_serv_sock_t *        tail;
+    gn_serv_sock_list_len_t len;
 } gn_serv_sock_list_t;
 
 #endif // GN_SERV_SOCK_LIST_T_H
