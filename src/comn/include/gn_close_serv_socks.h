a63d2b05d8d2b3861372e4e869dbc087fc2e2487
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-05-28 13:29:44 +0200

    Added function attribute

--- src/comn/include/gn_close_serv_socks.h
+++ src/comn/include/gn_close_serv_socks.h
@@ -7,13 +7,14 @@
 
 __attribute__((nonnull))
 void
-gn_close_serv_socks (gn_serv_sock_list_t * const serv_sock_list);
+gn_close_serv_socks (gn_serv_sock_list_t * const list);
 
 int
 gn_close (int * const fd);
 
+__attribute__((nonnull))
 __attribute__((warn_unused_result))
 gn_serv_sock_t *
-gn_serv_sock_list_pop_front (gn_serv_sock_list_t * const serv_sock_list);
+gn_serv_sock_list_pop_front (gn_serv_sock_list_t * const list);
 
 #endif // GN_CLOSE_SERV_SOCKS_H
--- src/comn/include/gn_serv_sock_list_pop_front.h
+++ src/comn/include/gn_serv_sock_list_pop_front.h
@@ -1,10 +1,11 @@
 #ifndef GN_SERV_SOCK_LIST_POP_FRONT_H
 #define GN_SERV_SOCK_LIST_POP_FRONT_H
 
-#include <stddef.h>
-
 #include <gn_serv_sock_list_t.h>
 
+#include <stddef.h>
+
+__attribute__((nonnull))
 __attribute__((warn_unused_result))
 gn_serv_sock_t *
 gn_serv_sock_list_pop_front (gn_serv_sock_list_t * const list);
