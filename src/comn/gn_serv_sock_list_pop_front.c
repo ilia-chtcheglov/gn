b6947274d92372c1c42a7c1d89ea22b725b6175f
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-05-28 13:29:13 +0200

    Simplified code

--- src/comn/gn_serv_sock_list_pop_front.c
+++ src/comn/gn_serv_sock_list_pop_front.c
@@ -1,32 +1,30 @@
 #include <gn_serv_sock_list_pop_front.h>
 
+__attribute__((nonnull))
 __attribute__((warn_unused_result))
 gn_serv_sock_t *
 gn_serv_sock_list_pop_front (gn_serv_sock_list_t * const list)
 {
+    gn_serv_sock_t * const this_ret = list->head;
+
     switch (list->len)
     {
         case 0:
-        {
             return NULL;
-        }
         case 1:
         {
-            gn_serv_sock_t * const ret = list->head;
             list->head = list->tail = NULL;
-            list->len--;
-            return ret;
+            break;
         }
         default:
         {
-            gn_serv_sock_t * const ret = list->head;
             list->head = list->head->next;
 
             list->head->prev = list->tail;
             list->tail->next = list->head;
-
-            list->len--;
-            return ret;
         }
     }
+
+    list->len--;
+    return this_ret;
 }
