b596cb1521444645b129bb0c66dafb7262e04ca2
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-05-28 15:39:48 +0200

    Simplified code

--- src/mstr/gn_vhst_conf_list_pop.c
+++ src/mstr/gn_vhst_conf_list_pop.c
@@ -5,29 +5,26 @@ __attribute__((warn_unused_result))
 gn_vhst_conf_t *
 gn_vhst_conf_list_pop (gn_vhst_conf_list_t * const list)
 {
+    gn_vhst_conf_t * const this_ret = list->head;
+
     switch (list->len)
     {
         case 0:
-        {
             return NULL;
-        }
         case 1:
         {
-            gn_vhst_conf_t * const ret = list->head;
             list->head = list->tail = NULL;
-            list->len--;
-            return ret;
+            break;
         }
         default:
         {
-            gn_vhst_conf_t * const ret = list->head;
             list->head = list->head->next;
 
             list->head->prev = list->tail;
             list->tail->next = list->head;
-
-            list->len--;
-            return ret;
         }
     }
+
+    list->len--;
+    return this_ret;
 }
