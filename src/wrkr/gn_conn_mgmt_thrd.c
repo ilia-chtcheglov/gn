ebad93592de607a52b3ee7242451a51b85ae0525
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-05-30 15:12:04 +0200

    Detected end of HTTP request headers

--- src/wrkr/gn_conn_mgmt_thrd.c
+++ src/wrkr/gn_conn_mgmt_thrd.c
@@ -233,40 +233,51 @@ gn_extr_hdrn (gn_conn_t * const conn)
     for ( ;
          recv_buf_i < conn->recv_buf_len &&
          conn->hdrn_len < conn->hdrn_sz - 1 &&
-         conn->recv_buf[recv_buf_i] != ':';
+         conn->recv_buf[recv_buf_i] != ':' &&
+         conn->recv_buf[recv_buf_i] != '\n';
          recv_buf_i++, conn->hdrn_len++)
     {
         conn->hdrn[conn->hdrn_len] = conn->recv_buf[recv_buf_i];
     }
-
     conn->hdrn[conn->hdrn_len] = '\0';
-    if (conn->recv_buf[recv_buf_i] == ':')
-    {
-        printf ("Header name (%u) \"%s\".\n", conn->hdrn_len, conn->hdrn);
-        // Move the rest of the data to the beginning of the receive buffer.
-        size_t i = 0;
-        size_t j = (size_t)conn->hdrn_len + 1;
-        while (j < conn->recv_buf_len)
-        {
-            conn->recv_buf[i] = conn->recv_buf[j];
-            i++;
-            j++;
-        }
-        conn->recv_buf_len -= (uint32_t)conn->hdrn_len + 1;
-        conn->recv_buf[conn->recv_buf_len] = '\0';
-        printf ("Remaining (%u) \"%s\"\n", conn->recv_buf_len, conn->recv_buf); // TODO: Remove.
 
-        conn->prev_step = GN_CONN_STEP_INVALID;
-        conn->step = GN_CONN_STEP_EXTR_HDRV; // TODO: Go to next step.
-    }
-    else
+    switch (conn->recv_buf[recv_buf_i])
     {
-        if (conn->hdrn_len == conn->hdrn_sz - 1)
+        case ':':
         {
-            fprintf (stderr, "Request header name too long.\n");
-            conn->step = GN_CONN_STEP_CLOSE;
+            printf ("Header name (%u) \"%s\".\n", conn->hdrn_len, conn->hdrn);
+            // Move the rest of the data to the beginning of the receive buffer.
+            size_t i = 0;
+            size_t j = (size_t)conn->hdrn_len + 1;
+            while (j < conn->recv_buf_len)
+            {
+                conn->recv_buf[i] = conn->recv_buf[j];
+                i++;
+                j++;
+            }
+            conn->recv_buf_len -= (uint32_t)conn->hdrn_len + 1;
+            conn->recv_buf[conn->recv_buf_len] = '\0';
+            printf ("Remaining (%u) \"%s\"\n", conn->recv_buf_len, conn->recv_buf); // TODO: Remove.
+
+            conn->step = GN_CONN_STEP_EXTR_HDRV; // TODO: Go to next step.
+            break;
         }
-        else conn->step = GN_CONN_STEP_RECV_DATA;
+        case '\n':
+        {
+            printf ("End of request headers.\n");
+            conn->recv_buf[0] = '\0';
+            conn->step = GN_CONN_STEP_RECV_DATA;
+            break;
+        }
+        default:
+        {
+            if (conn->hdrn_len == conn->hdrn_sz - 1)
+            {
+                fprintf (stderr, "Request header name too long.\n");
+                conn->step = GN_CONN_STEP_CLOSE;
+            }
+            else conn->step = GN_CONN_STEP_RECV_DATA;
+        }
     }
 }
 
