039e047a0e5a7b13a41ddb203c798f5f57f77ff5
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-05-30 11:38:15 +0200

    Removed HTTP protocol from receive buffer

--- src/wrkr/gn_conn_mgmt_thrd.c
+++ src/wrkr/gn_conn_mgmt_thrd.c
@@ -188,13 +188,26 @@ gn_extr_prot (gn_conn_t * const conn)
     conn->prot[conn->prot_len] = '\0';
     if (conn->recv_buf[recv_buf_i] == '\n')
     {
+        // Move the rest of the data to the beginning of the receive buffer.
+        size_t i = 0;
+        size_t j = (size_t)conn->prot_len + 1;
+        while (j < conn->recv_buf_len)
+        {
+            conn->recv_buf[i] = conn->recv_buf[j];
+            i++;
+            j++;
+        }
+        conn->recv_buf_len -= (uint32_t)conn->prot_len + 1;
+        conn->recv_buf[conn->recv_buf_len] = '\0';
+        printf ("Remaining (%u) \"%s\"\n", conn->recv_buf_len, conn->recv_buf); // TODO: Remove.
+
         if (conn->prot_len > 0 && conn->prot[conn->prot_len - 1] == '\r')
         {
             conn->prot[conn->prot_len - 1] = '\0';
             conn->prot_len--;
         }
-
         printf ("Request protocol (%u) \"%s\".\n", conn->prot_len, conn->prot);
+
         conn->prev_step = GN_CONN_STEP_INVALID;
         conn->step = GN_CONN_STEP_RECV_DATA; // TODO: Go to next step.
     }
