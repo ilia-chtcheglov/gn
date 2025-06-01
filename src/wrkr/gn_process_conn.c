9bfcba9cadc86540c4807901dc137fee90237348
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-06-01 09:43:51 +0200

    Added error checks

--- src/wrkr/gn_process_conn.c
+++ src/wrkr/gn_process_conn.c
@@ -176,26 +176,61 @@ __attribute__((nonnull))
 void
 gn_send_file (gn_conn_t * const conn)
 {
-    printf ("Reading file. Buffer %p: %u/%u. Adding to index %u (max %u).\n",
-            conn->send_buf,
-            conn->send_buf_len,
-            conn->send_buf_sz,
-            conn->send_buf_len,
-            conn->send_buf_sz - conn->send_buf_len - 1);
-
-    const ssize_t rread = read (conn->fd, &conn->send_buf[conn->send_buf_len],
-                                conn->send_buf_sz - conn->send_buf_len - 1);
+    const size_t max_read = conn->send_buf_sz - conn->send_buf_len - 1;
+    const ssize_t rread = read (conn->fd, &conn->send_buf[conn->send_buf_len], max_read);
     switch (rread)
     {
         case -1:
         {
+            /*
+             * Documentation says: On error, -1 is returned, and errno is set appropriately.
+             * IN THIS CASE it is left unspecified whether the file position (if any) changes.
+             */
             switch (errno)
             {
+                case EIO: // Could be a low-level I/O error.
+                    break;
                 case EAGAIN:
+                /*
+                 * read() can return EAGAIN or EWOULDBLOCK.
+                 * But if they have the same value we can use only one of them.
+                 */
+                #if EAGAIN != EWOULDBLOCK
+                case EWOULDBLOCK:
+                #endif
                 case EINTR:
-                case EIO:
                 {
                     conn->last_io = time (NULL);
+                    break;
+                }
+                case EBADF:
+                {
+                    fprintf (stderr, "Failed to read file. %s.\n", strerror (errno));
+
+                    /*
+                     * If conn->fd is not invalid (> -1) but we get EBADF it means the fd value was changed/corrupted.
+                     * Close the connection without closing conn->fd. We don't know what fd refers to now.
+                     * If fd is invalid (< 0), I don't know why gn_send_file() was called. Close the connection anyway.
+                     */
+                    if (conn->fd > -1)
+                    {
+                        conn->fd = -1;
+                        // TODO: Stop the worker process.
+                    }
+                    conn->step = GN_CONN_STEP_CLOSE;
+                    break;
+                }
+                case EISDIR:
+                {
+                    fprintf (stderr, "Failed to read file. %s.\n", strerror (errno));
+
+                    /*
+                     * This program doesn't open directories. If we get EISDIR it means
+                     * the fd value was changed/corrupted. Close the connection without
+                     * closing conn->fd. We don't know what fd refers to now.
+                     */
+                    conn->fd = -1;
+                    conn->step = GN_CONN_STEP_CLOSE;
                     break;
                 }
                 default:
@@ -208,8 +243,12 @@ gn_send_file (gn_conn_t * const conn)
         }
         case 0:
         {
-            conn->step = GN_CONN_STEP_CLOSE;
-            break;
+            if (max_read > 0)
+            {
+                conn->step = GN_CONN_STEP_CLOSE;
+                break;
+            }
+            __attribute__((fallthrough));
         }
         default:
         {
