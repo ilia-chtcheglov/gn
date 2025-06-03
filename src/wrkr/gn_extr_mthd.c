959f7ec7f0e2ae91a6c5e149bea31bdb907938fa
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-06-03 18:22:01 +0200

    Send 501 response code

--- src/wrkr/gn_extr_mthd.c
+++ src/wrkr/gn_extr_mthd.c
@@ -1,5 +1,7 @@
 #include <gn_extr_mthd.h>
 
+#include <string.h>
+
 __attribute__((nonnull))
 void
 gn_extr_mthd (gn_conn_t * const conn)
@@ -29,17 +31,23 @@ gn_extr_mthd (gn_conn_t * const conn)
         }
         conn->recv_buf.len -= conn->mthd.len + 1;
         conn->recv_buf.dat[conn->recv_buf.len] = '\0';
-        printf ("Remaining (%u) \"%s\"\n", conn->recv_buf.len, conn->recv_buf.dat); // TODO: Remove.
+        // printf ("Remaining (%u) \"%s\"\n", conn->recv_buf.len, conn->recv_buf.dat); // TODO: Remove.
 
         conn->prev_step = GN_CONN_STEP_INVALID;
-        conn->step = GN_CONN_STEP_EXTR_URI; // TODO: Go to next step.
+        if (conn->mthd.len == 0 || strcmp (conn->mthd.dat, "GET") != 0)
+        {
+            conn->status = 501;
+            conn->step = GN_CONN_STEP_WRIT_HDRS;
+        }
+        else conn->step = GN_CONN_STEP_EXTR_URI; // TODO: Go to next step.
     }
     else
     {
         if (conn->mthd.len == conn->mthd.sz - 1)
         {
             fprintf (stderr, "Request method too long.\n"); // TODO: Remove.
-            conn->step = GN_CONN_STEP_CLOSE;
+            conn->status = 501;
+            conn->step = GN_CONN_STEP_WRIT_HDRS;
         }
         else conn->step = GN_CONN_STEP_RECV_DATA;
     }
