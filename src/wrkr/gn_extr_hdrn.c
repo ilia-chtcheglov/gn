9590ce5912da99b9cc5a8a741bb58128512e39f9
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-06-03 19:19:36 +0200

    Detect missing Host header

--- src/wrkr/gn_extr_hdrn.c
+++ src/wrkr/gn_extr_hdrn.c
@@ -32,6 +32,15 @@ gn_htbl_dump (gn_htbl_t * const tbl)
     printf ("\n");
 }
 
+#include <ctype.h>
+
+__attribute__((nonnull))
+__attribute__((warn_unused_result))
+gn_htbl_item_t *
+gn_htbl_srch (gn_htbl_t * const tbl,
+              const char * const key,
+              gn_str_len_t key_len);
+
 __attribute__((nonnull))
 void
 gn_extr_hdrn (gn_conn_t * const conn)
@@ -44,7 +53,11 @@ gn_extr_hdrn (gn_conn_t * const conn)
          conn->recv_buf.dat[recv_buf_i] != '\n';
          recv_buf_i++, conn->hdrn.len++)
     {
-        conn->hdrn.dat[conn->hdrn.len] = conn->recv_buf.dat[recv_buf_i];
+        /*
+         * From RFC9112 5. Field Syntax:
+         * Each field line consists of a case-insensitive field name...
+         */
+        conn->hdrn.dat[conn->hdrn.len] = (char)tolower ((unsigned char)conn->recv_buf.dat[recv_buf_i]);
     }
     conn->hdrn.dat[conn->hdrn.len] = '\0';
 
@@ -73,7 +86,21 @@ gn_extr_hdrn (gn_conn_t * const conn)
         {
             printf ("End of request headers.\n");
             conn->recv_buf.dat[0] = '\0';
-            gn_htbl_dump (&conn->req_hdrs);
+            gn_htbl_dump (&conn->req_hdrs); // TODO: Remove.
+
+            /*
+             * From RFC9112 3.2 Request Target:
+             * A server MUST response with a 400 (Bad Request) status code to
+             * any HTTP/1.1 request message that lacks a Host header field...
+             */
+            if (gn_htbl_srch (&conn->req_hdrs, "host", 4) == NULL)
+            {
+                fprintf (stderr, "Missing Host header.\n");
+                conn->status = 400;
+                conn->step = GN_CONN_STEP_WRIT_HDRS;
+                return;
+            }
+
             conn->step = GN_CONN_STEP_OPEN_FILE;
             break;
         }
