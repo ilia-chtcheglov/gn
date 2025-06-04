1a38019cefc6d725c9bb5d2745ee74208cabd537
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-06-04 19:31:45 +0200

    Removed debug lines

--- src/comn/gn_htbl_srch.c
+++ src/comn/gn_htbl_srch.c
@@ -16,44 +16,30 @@ gn_htbl_srch (gn_htbl_t * const tbl,
 
     // Compute the key hash and get a table index.
     const gn_htbl_len_t index = gn_htbl_hash (tbl->sz, key, key_len);
-    printf ("Searching key at index: %u. Key: \"%s\"\n", index, key); // TODO: Remove.
 
-    if (tbl->itms[index] == NULL)
+    if (tbl->itms[index] == NULL) return NULL;
+
+    // There's a hash table item/key at computed index.
+    // Is the first item the one we have to find ?
+    gn_htbl_item_t * next_item = tbl->itms[index];
+    if (next_item->key_len == key_len && strncmp (next_item->key, key, next_item->key_len) == 0)
     {
-        // There's no hash table item/key at computed index.
-        fprintf (stderr, "Key \"%s\" not found at index %u.\n", key, index);
+        // Yes, the key we have to find is the first at this index.
+        return next_item;
     }
-    else
+    
+    // No, the key we have to find is not the first at this index.
+    // Find the key in the list. Start at the second item.
+    next_item = next_item->next;
+    for ( ; next_item != NULL; next_item = next_item->next)
     {
-        // There's a hash table item/key at computed index.
-        // Is the first item the one we have to find ?
-        gn_htbl_item_t * next_item = tbl->itms[index];
         if (next_item->key_len == key_len && strncmp (next_item->key, key, next_item->key_len) == 0)
         {
-            // Yes, the key we have to find is the first at this index.
-            printf ("FOUND '%s'\n", next_item->val); // TODO: Remove.
+            // We found the key. Now return it.
             return next_item;
         }
-        else
-        {
-            // No, the key we have to find is not the first at this index.
-            // Find the key in the list. Start at the second item.
-            next_item = next_item->next;
-            for ( ; next_item != NULL; next_item = next_item->next)
-            {
-                printf ("\t.key (%u) \"%s\", .val (%u) \"%s\"\n",
-                        next_item->key_len, next_item->key,
-                        next_item->val_len, next_item->val);
-                if (next_item->key_len == key_len && strncmp (next_item->key, key, next_item->key_len) == 0)
-                {
-                    printf ("FOUND '%s'\n", next_item->val); // TODO: Remove.
-                    // We found the key. Now return it.
-                    return next_item;
-                }
-            }
-            // We didn't find the key.
-        }
     }
+    // We didn't find the key.
 
     return NULL;
 }
