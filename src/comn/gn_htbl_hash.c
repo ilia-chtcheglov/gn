87e4f8e64e95ff74c70c048f5f6d52fddad16f24
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-06-04 19:39:05 +0200

    Updated hashing formula

--- src/comn/gn_htbl_hash.c
+++ src/comn/gn_htbl_hash.c
@@ -1,17 +1,18 @@
 #include <gn_htbl_hash.h>
 
 __attribute__((nonnull))
+__attribute__((pure))
 __attribute__((warn_unused_result))
 gn_htbl_len_t
 gn_htbl_hash (const gn_htbl_len_t size,
               const char * const key,
-              const uint32_t key_len)
+              const gn_str_len_t key_len)
 {
-    gn_htbl_len_t hash = 0;
-    for (uint32_t i = 0; i < key_len; i++)
+    size_t hash = 0;
+    for (gn_str_len_t i = 0; i < key_len; i++)
     {
-        hash += (gn_htbl_len_t)((double)key[i] * pow (10, (double)i));
+        hash = hash * 31 + (unsigned char)key[i];
     }
 
-    return hash % size;
+    return (gn_htbl_len_t)hash % size;
 }
--- src/comn/include/gn_htbl_hash.h
+++ src/comn/include/gn_htbl_hash.h
@@ -2,14 +2,15 @@
 #define GN_HTBL_HASH_H
 
 #include <gn_htbl_t.h>
+#include <gn_str_t.h>
 
-#include <math.h>
+#include <stddef.h>
 
 __attribute__((nonnull))
 __attribute__((warn_unused_result))
 gn_htbl_len_t
 gn_htbl_hash (const gn_htbl_len_t size,
               const char * const key,
-              const uint32_t key_len);
+              const gn_str_len_t key_len);
 
 #endif // GN_HTBL_HASH_H
