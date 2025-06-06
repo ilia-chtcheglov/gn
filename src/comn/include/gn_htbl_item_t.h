5f45c9b12181d39263663ca39e88641f35f0c934
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-06-06 17:17:05 +0200

    Changed variables type

--- src/comn/include/gn_htbl_item_t.h
+++ src/comn/include/gn_htbl_item_t.h
@@ -1,16 +1,16 @@
 #ifndef GN_HTBL_ITEM_T_H
 #define GN_HTBL_ITEM_T_H
 
-#include <stdint.h>
+#include <gn_str_t.h>
 
 typedef struct gn_htbl_item_t gn_htbl_item_t;
 
 struct gn_htbl_item_t
 {
     char *           key;
-    uint32_t         key_len;
+    gn_str_len_t     key_len;
     char *           val;
-    uint32_t         val_len;
+    gn_str_len_t     val_len;
     gn_htbl_item_t * prev;
     gn_htbl_item_t * next;
 };
