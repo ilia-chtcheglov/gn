6b5267f48a9d5d0d3655b55541f44390cb045587
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-06-09 11:48:56 +0200

    Changed error handling

--- src/wrkr/gn_extr_mthd.c
+++ src/wrkr/gn_extr_mthd.c
@@ -12,40 +12,50 @@ gn_extr_mthd (gn_conn_t * const conn)
 
         conn->mthd.dat[conn->mthd.len++] = conn->recv_buf.dat[recv_buf_i++];
     }
-
     conn->mthd.dat[conn->mthd.len] = '\0';
+
     if (conn->recv_buf.dat[recv_buf_i] == ' ')
     {
-        printf ("Request method (%u) \"%s\".\n", conn->mthd.len, conn->mthd.dat); // TODO: Remove.
         // Move the rest of the data to the beginning of the receive buffer.
-        gn_str_len_t i = 0;
-        gn_str_len_t j = conn->mthd.len + 1;
-        while (j < conn->recv_buf.len)
-        {
-            conn->recv_buf.dat[i] = conn->recv_buf.dat[j];
-            i++;
-            j++;
-        }
+        recv_buf_i = 0;
+        gn_str_len_t i = conn->mthd.len + 1;
+        while (i < conn->recv_buf.len) conn->recv_buf.dat[recv_buf_i++] = conn->recv_buf.dat[i++];
+
         conn->recv_buf.len -= conn->mthd.len + 1;
         conn->recv_buf.dat[conn->recv_buf.len] = '\0';
-        // printf ("Remaining (%u) \"%s\"\n", conn->recv_buf.len, conn->recv_buf.dat); // TODO: Remove.
 
         conn->prev_step = GN_CONN_STEP_INVALID;
-        if (conn->mthd.len == 0 || strcmp (conn->mthd.dat, "GET") != 0)
+        if (conn->mthd.len == 0)
         {
-            conn->status = 501;
+            // TODO: Maybe log error.
+            conn->status = 400;
             conn->step = GN_CONN_STEP_WRIT_HDRS;
+            return;
         }
-        else conn->step = GN_CONN_STEP_EXTR_URI; // TODO: Go to next step.
+
+        if (strcmp (conn->mthd.dat, "GET") == 0 ||
+            strcmp (conn->mthd.dat, "POST") == 0 ||
+            strcmp (conn->mthd.dat, "HEAD") == 0)
+        {
+            conn->step = GN_CONN_STEP_EXTR_URI;
+            return;
+        }
+
+        // TODO: Maybe log error.
+        conn->status = 501;
+        conn->step = GN_CONN_STEP_WRIT_HDRS;
+        return;
     }
-    else
+
+    if (conn->mthd.len == conn->mthd.sz - 1)
     {
-        if (conn->mthd.len == conn->mthd.sz - 1)
-        {
-            fprintf (stderr, "Request method too long.\n"); // TODO: Remove.
-            conn->status = 501;
-            conn->step = GN_CONN_STEP_WRIT_HDRS;
-        }
-        else conn->step = GN_CONN_STEP_RECV_DATA;
+        // TODO: Maybe log error.
+        conn->status = 400; // TODO: Return 400 Bad Request or 501 Not Implemented?
+        conn->prev_step = GN_CONN_STEP_INVALID;
+        conn->step = GN_CONN_STEP_WRIT_HDRS;
+        return;
     }
+
+    conn->prev_step = GN_CONN_STEP_EXTR_MTHD;
+    conn->step = GN_CONN_STEP_RECV_DATA;
 }
