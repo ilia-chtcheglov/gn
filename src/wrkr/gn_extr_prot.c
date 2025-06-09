381a69807f6cb04bea9f1369b09647278ccd4f17
Ilia Chtcheglov <90645986+ilia-chtcheglov@users.noreply.github.com>
2025-06-09 13:45:45 +0200

    Changed error handling

--- src/wrkr/gn_extr_prot.c
+++ src/wrkr/gn_extr_prot.c
@@ -12,41 +12,53 @@ gn_extr_prot (gn_conn_t * const conn)
 
         conn->prot.dat[conn->prot.len++] = conn->recv_buf.dat[recv_buf_i++];
     }
-
     conn->prot.dat[conn->prot.len] = '\0';
+
     if (conn->recv_buf.dat[recv_buf_i] == '\n')
     {
         // Move the rest of the data to the beginning of the receive buffer.
-        size_t i = 0;
-        size_t j = (size_t)conn->prot.len + 1;
-        while (j < conn->recv_buf.len)
-        {
-            conn->recv_buf.dat[i] = conn->recv_buf.dat[j];
-            i++;
-            j++;
-        }
-        conn->recv_buf.len -= (uint32_t)conn->prot.len + 1;
-        conn->recv_buf.dat[conn->recv_buf.len] = '\0';
-        // printf ("Remaining (%u) \"%s\"\n", conn->recv_buf.len, conn->recv_buf.dat); // TODO: Remove.
+        recv_buf_i = 0;
+        gn_str_len_t i = conn->prot.len + 1;
+        while (i < conn->recv_buf.len) conn->recv_buf.dat[recv_buf_i++] = conn->recv_buf.dat[i++];
 
-        // Must end with CRLF.
-        if (conn->prot.len > 0 && conn->prot.dat[conn->prot.len - 1] == '\r')
-        {
-            conn->prot.dat[conn->prot.len - 1] = '\0';
-            conn->prot.len--;
-        }
-        printf ("Request protocol (%u) \"%s\".\n", conn->prot.len, conn->prot.dat);
+        conn->recv_buf.len -= conn->prot.len + 1;
+        conn->recv_buf.dat[conn->recv_buf.len] = '\0';
 
         conn->prev_step = GN_CONN_STEP_INVALID;
-        conn->step = GN_CONN_STEP_EXTR_HDRN; // TODO: Go to next step.
-    }
-    else
-    {
-        if (conn->prot.len == conn->prot.sz - 1)
+        /*
+         * From RFC 9112 2.2 Message Parsing:
+         * Although the line terminator for the start-line and fields is
+         * the sequence CRLF, a recipient MAY recognize a single LF as
+         * a line terminator and ignore any preceding CR.
+         */
+        // Remove CR from the end of the request protocol buffer.
+        if (conn->prot.len > 0 && conn->prot.dat[conn->prot.len - 1] == '\r')
         {
-            fprintf (stderr, "Request protocol too long.\n");
-            conn->step = GN_CONN_STEP_CLOSE;
+            conn->prot.len--;
+            conn->prot.dat[conn->prot.len] = '\0';
         }
-        else conn->step = GN_CONN_STEP_RECV_DATA;
+
+        if (conn->prot.len == 0)
+        {
+            // TODO: Maybe log error.
+            conn->status = 400;
+            conn->step = GN_CONN_STEP_WRIT_HDRS;
+            return;
+        }
+
+        conn->step = GN_CONN_STEP_EXTR_HDRN;
+        return;
     }
+
+    if (conn->prot.len == conn->prot.sz - 1)
+    {
+        // TODO: Maybe log error.
+        conn->status = 400;
+        conn->prev_step = GN_CONN_STEP_INVALID;
+        conn->step = GN_CONN_STEP_WRIT_HDRS;
+        return;
+    }
+
+    conn->prev_step = GN_CONN_STEP_EXTR_PROT;
+    conn->step = GN_CONN_STEP_RECV_DATA;
 }
